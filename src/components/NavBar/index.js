73566ba40239dd54ba36cff878ddcd46560c6bb6
Miriamvdb <miriamvdb@live.nl>
2022-07-18 15:38:35 +0200

    Added navbar

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -1,13 +1,64 @@
 import { NavLink } from "react-router-dom";
+import { FiUser, FiShoppingCart, FiHeart } from "react-icons/fi";
 import "./styles.css";
 
 const NavBar = () => {
   return (
-    <div className="NavBarContainer">
-      <div className="NavBarLinks">
-        <NavLink to="/">Home</NavLink>
-        <NavLink to="/shop">Shop</NavLink>
-        <NavLink to="/details/:id">About</NavLink>
+    <div className="NavContainer">
+      <div className="NavLinksAndInput">
+        <div className="NavLinks">
+          <h1 style={{ color: "white" }}>GM</h1>
+          <NavLink
+            className="link"
+            style={({ isActive }) =>
+              isActive ? { color: "white", fontWeight: "bold" } : undefined
+            }
+            to="/"
+          >
+            Home
+          </NavLink>
+          <NavLink
+            className="link"
+            style={({ isActive }) =>
+              isActive ? { color: "white", fontWeight: "bold" } : undefined
+            }
+            to="/shop"
+          >
+            Shop
+          </NavLink>
+          <NavLink
+            className="link"
+            style={({ isActive }) =>
+              isActive ? { color: "white", fontWeight: "bold" } : undefined
+            }
+            to="/details/:id"
+          >
+            About
+          </NavLink>
+        </div>
+        <div className="NavInput">
+          <input className="searchbox" placeholder="Find product" />
+        </div>
+      </div>
+      <div className="NavIcons">
+        <NavLink
+          style={({ isActive }) => (isActive ? { color: "white" } : undefined)}
+          to="/login"
+        >
+          <FiUser className="icon" />
+        </NavLink>
+        <NavLink
+          style={({ isActive }) => (isActive ? { color: "white" } : undefined)}
+          to="/shoppingcard"
+        >
+          <FiShoppingCart className="icon" />
+        </NavLink>
+        <NavLink
+          style={({ isActive }) => (isActive ? { color: "white" } : undefined)}
+          to="/favorites"
+        >
+          <FiHeart className="icon" />
+        </NavLink>
       </div>
     </div>
   );
