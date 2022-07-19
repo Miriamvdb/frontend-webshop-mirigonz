9d1437fe9f953bc5238a1c176216ec1a4dd6026e
gonzalodutto <gonzalodutto@gmail.com>
2022-07-19 12:29:04 +0200

    feat/filterList - filterListComponent created

--- src/components/FilterList/index.js
+++ src/components/FilterList/index.js
@@ -2,23 +2,17 @@ import "./styles.css";
 
 const FilterList = (props) => {
   return (
-    <div className="FilterListComponent">
-      <div>
-        <div>
-          <img src={props.image} alt={"logo"} className="card-img" />
-        </div>
-      </div>
-      <div className="card-text-section">
-        <div className="product-title">
-          <b>{props.title}</b>
-        </div>{" "}
-        <div className="price-rating-container">
-          <b className="price">€{props.price}</b>
-          <b>{props.rating} ★★★☆☆</b>
-        </div>
-        <div className="product-description">
-          <p>{props.description.substring(0, 150)}...</p>
-        </div>
+    <div className="filters-child">
+      <b>{props.title} </b>
+      <div className="list">
+        {props.values
+          ? props.values.map((product, i) => (
+              <label key={i}>
+                <input type="checkbox" />
+                {product}
+              </label>
+            ))
+          : ""}
       </div>
     </div>
   );
--- src/pages/ProductListPage/index.js
+++ src/pages/ProductListPage/index.js
@@ -1,7 +1,14 @@
 import "./styles.css";
 import { useEffect, useState } from "react";
 import axios from "axios";
-import { ProductCard } from "../../components";
+import { ProductCard, FilterList } from "../../components";
+
+const filter1 = {
+  title: "Categories",
+  values: ["Hola", "Bla", "Perro", "Miriam", "TImo"],
+};
+const filter2 = { title: "Rating Item", values: ["Hola", "Bla", "Perro"] };
+const filter3 = { title: "Price Filter", values: ["Hola", "Bla", "Perro"] };
 
 const ProductListPage = () => {
   const [productList, setProductList] = useState(null);
@@ -19,59 +26,10 @@ const ProductListPage = () => {
     <div className="ProductListPage">
       <div className="Filters-ProductList">
         <div className="filters">
-          <div className="filters-child">
-            <b>Categories</b>
-            <div className="list">
-              <label>
-                <input type="checkbox" />
-                My Value
-              </label>
-              <label>
-                <input type="checkbox" />
-                My Value
-              </label>
-              <label>
-                <input type="checkbox" />
-                My Value
-              </label>
-            </div>
-          </div>
-          <div className="filters-child">
-            <b>Categories</b>
-            <div className="list">
-              <label>
-                <input type="checkbox" />
-                My Value
-              </label>
-              <label>
-                <input type="checkbox" />
-                My Value
-              </label>
-              <label>
-                <input type="checkbox" />
-                My Value
-              </label>
-            </div>
-          </div>
-          <div className="filters-child">
-            <b>Categories</b>
-            <div className="list">
-              <label>
-                <input type="checkbox" />
-                My Value
-              </label>
-              <label>
-                <input type="checkbox" />
-                My Value
-              </label>
-              <label>
-                <input type="checkbox" />
-                My Value
-              </label>
-            </div>
-          </div>
+          <FilterList title={filter1.title} values={filter1.values} />
+          <FilterList title={filter2.title} values={filter2.values} />
+          <FilterList title={filter3.title} values={filter3.values} />
         </div>
-
         <div>
           {productList
             ? productList.map((product, i) => (
