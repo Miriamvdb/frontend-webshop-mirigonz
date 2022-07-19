5e5ffff97e4c36c3fcf4094a1e0707be1202df69
gonzalodutto <gonzalodutto@gmail.com>
2022-07-19 10:53:57 +0200

    Initial Commit

--- src/pages/ProductListPage/index.js
+++ src/pages/ProductListPage/index.js
@@ -1,9 +1,100 @@
 import "./styles.css";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { ProductCard } from "../../components";
 
 const ProductListPage = () => {
+  const [productList, setProductList] = useState(null);
+
+  useEffect(() => {
+    async function getProducts() {
+      const productResponse = await axios.get("http://localhost:4000/products");
+      setProductList(productResponse.data);
+      // console.log(productResponse);
+    }
+
+    getProducts();
+    // if (params.filter) {
+    //   setFilter(params.filter);
+    // } else {
+    //   setFilter("");
+    // }
+  }, []);
+
   return (
     <div className="ProductListPage">
-      <h1>List of all products</h1>
+      <div className="Filters-ProductList">
+        <div className="filters">
+          <div className="filters-child">
+            <b>Categories</b>
+            <div className="list">
+              <label>
+                <input type="checkbox" />
+                My Value
+              </label>
+              <label>
+                <input type="checkbox" />
+                My Value
+              </label>
+              <label>
+                <input type="checkbox" />
+                My Value
+              </label>
+            </div>
+          </div>
+          <div className="filters-child">
+            <b>Categories</b>
+            <div className="list">
+              <label>
+                <input type="checkbox" />
+                My Value
+              </label>
+              <label>
+                <input type="checkbox" />
+                My Value
+              </label>
+              <label>
+                <input type="checkbox" />
+                My Value
+              </label>
+            </div>
+          </div>
+          <div className="filters-child">
+            <b>Categories</b>
+            <div className="list">
+              <label>
+                <input type="checkbox" />
+                My Value
+              </label>
+              <label>
+                <input type="checkbox" />
+                My Value
+              </label>
+              <label>
+                <input type="checkbox" />
+                My Value
+              </label>
+            </div>
+          </div>
+        </div>
+
+        <div>
+          {productList
+            ? productList.map((product, i) => (
+                <ProductCard
+                  key={i}
+                  id={product.id}
+                  title={product.title}
+                  price={product.price}
+                  description={product.description}
+                  rating={product.rating}
+                  image={product.mainImage}
+                  categoryId={product.categoryId}
+                />
+              ))
+            : ""}
+        </div>
+      </div>
     </div>
   );
 };
