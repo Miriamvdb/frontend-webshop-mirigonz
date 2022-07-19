ba65b30edb16ddba6e9414aad6ae3cddb1ddb286
Miriamvdb <miriamvdb@live.nl>
2022-07-19 15:11:10 +0200

    Added LowerSlice on detailspage

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,5 @@
 export * from "./NavBar";
 export * from "./ProductCard";
 export * from "./FilterList";
+export * from "./LowerSlice";
 export * from "./Footer";
--- src/pages/DetailsPage/index.js
+++ src/pages/DetailsPage/index.js
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 import { useParams } from "react-router-dom";
 import "./styles.css";
+import { LowerSlice } from "../../components";
 
 const DetailsPage = () => {
   const [products, setProducts] = useState({});
@@ -19,29 +20,32 @@ const DetailsPage = () => {
   }, [params.id]);
 
   return (
-    <div className="ContainerDetailsPage">
-      {!products ? (
-        "Loading.."
-      ) : (
-        <div className="AllProductDetails">
-          <div>
-            <img
-              className="ProductImage"
-              src={products.mainImage}
-              alt={products.title}
-            />
+    <div>
+      <div className="ContainerDetailsPage">
+        {!products ? (
+          "Loading.."
+        ) : (
+          <div className="AllProductDetails">
+            <div>
+              <img
+                className="ProductImage"
+                src={products.mainImage}
+                alt={products.title}
+              />
+            </div>
+            <div className="ProductDescr">
+              <h2>{products.title}</h2>
+              <h2>€{products.price}</h2>
+              <p>{products.description}</p>
+              <p>
+                <b>Rating:</b> {products.rating} <b>Category:</b>{" "}
+                {products.categoryId}
+              </p>
+            </div>
           </div>
-          <div className="ProductDescr">
-            <h2>{products.title}</h2>
-            <h2>€{products.price}</h2>
-            <p>{products.description}</p>
-            <p>
-              <b>Rating:</b> {products.rating} <b>Category:</b>{" "}
-              {products.categoryId}
-            </p>
-          </div>
-        </div>
-      )}
+        )}
+      </div>
+      <LowerSlice />
     </div>
   );
 };
