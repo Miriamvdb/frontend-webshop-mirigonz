c4a0a2302a38f97b9148165579bb08e6c1577103
gonzalodutto <gonzalodutto@gmail.com>
2022-07-20 16:12:18 +0200

    Bob

--- src/pages/AboutPage/index.js
+++ src/pages/AboutPage/index.js
@@ -3,7 +3,16 @@ import "./styles.css";
 const AboutPage = () => {
   return (
     <div className="AboutPage">
-      <h1>About Page</h1>
+      <div className="text-area">
+        <h1>About Page</h1>
+        <h3>We work hard to make you happy.</h3>
+      </div>
+      <div>
+        <img
+          src="https://www.pngall.com/wp-content/uploads/9/SpongeBob-TV-Series.png"
+          alt="bob"
+        />
+      </div>
     </div>
   );
 };
